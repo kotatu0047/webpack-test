ce7841b098eaabf8ab19ba63258b6a7d83ac5d87
yamamichi <yamamichi1995@outlook.jp>
2019-11-11 12:58:15 +0900

    fix

--- src/consts.js
+++ src/consts.js
@@ -7,7 +7,7 @@ export const r = vw(10)
 
 export const mainColor = 'rgb(29 ,233 ,192)'
 export const mainColorSkeleton = 'rgba(88,255,223,0.8)'
-export const subColor = 'rgba(4 ,93 ,86)'
+export const subColor = 'rgb(4 ,93 ,86)'
 export const subColorLite = '#04d7d0'
 export const blueSkeleton = 'rgba(57, 163, 235, 0.4)'
 export const red = 'rgb(255, 82, 82)'
