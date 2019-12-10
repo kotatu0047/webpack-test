f1883986bdf3e8a533a94711e70cbeeb9ff1dd1b
yamamichi <yamamichi1995@outlook.jp>
2019-12-10 10:33:52 +0900

    edit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { hot } from 'react-hot-loader/root'
+import { Button } from 'react-lamunanimation-buttons'
 import FavButton from './FavButton'
 
-const MyApp = hot(FavButton)
+const Dom = () => {
+  const handle = () => alert('fdsads')
+
+  return (
+    <div>
+      <Button onClick={handle} text="fdgdsafds" />
+    </div>
+  )
+}
+
+const MyApp = hot(Dom)
 
 ReactDOM.render(<MyApp />, document.getElementById('root'))
